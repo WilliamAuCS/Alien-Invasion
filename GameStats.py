b5d33ad1a66aff68d5f5959e0d6b8c49062af7cc
William Au <WilliamAuCS@gmail.com>
2019-09-26 22:50:14 -0700

    Initial commit

---
+++ GameStats.py
@@ -0,0 +1,27 @@
+class GameStats:
+    """Tracks Game stats"""
+
+    def __init__(self, ai_settings):
+        """Initialize statistics"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+
+        # Starting game in an active state
+        self.game_active = True
+
+        self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
+        self.reset_stats()
+
+        # Starting game in an inactive state
+        self.game_active = False
+
+        # High score
+        self.high_score = 0
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game"""
+        self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
